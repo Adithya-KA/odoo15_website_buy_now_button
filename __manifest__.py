cd09cc09a75b31f5cb3c2a990bb93aed37a9ebac
Adithya-KA <121038154+Adithya-KA@users.noreply.github.com>
2022-12-19 22:43:32 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Website Buy Now',
+    'version': '15.0.0.1.0',
+    'description': 'Buy Now',
+    'depends': [
+        'base', 'website_sale'
+    ],
+    'data': [
+        'views/button_view.xml'
+
+    ],
+
+    'installable': True,
+    'application': True
+
+}
